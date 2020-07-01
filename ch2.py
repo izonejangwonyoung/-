74ca35e6a63ae601e3201d51b347016fe1a5db22
izonejangwonyoung <2000shim@naver.com>
2020-07-01 20:21:38 +0900

    ..

---
+++ ch2.py
@@ -0,0 +1,22 @@
+# import keyword
+# print(keyword.kwlist)
+#
+# 키워드 확인법
+#
+# 변수는 변하는 수라는 뜻... 값 저장 가능한 메모리 상 공간 의미
+# 특정 데이터 형식과 레이블 사용하여 선언함
+# 변수의 명명은 키워드의 규칙을 따름
+
+print(type(100))
+print(type((1,2,3,4,5)))
+
+a=1234
+print(a)
+b=200
+print(b)
+b
+
+str="heloo"
+print(type(str))
+
+print(str[1:3])
